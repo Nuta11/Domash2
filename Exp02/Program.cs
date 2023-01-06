b9c643a87913d5d251d724c63ae849756ee31bde
Anna N <nearonskaya@yandex.ru>
2023-01-06 16:12:43 +0300

    решение задачи какое число большее, а какое меньшее.

---
+++ Exp02/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine( a+ " > " + b);
+}
+else if (b > a)
+{
+    Console.WriteLine( b+ " > " + a);
+}
+else
+{
+    Console.WriteLine( a+ " = " + b);
+}
