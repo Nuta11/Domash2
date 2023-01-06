9dded42ff2aedf618a9204850b98afe62d295046
Anna N <nearonskaya@yandex.ru>
2023-01-06 17:22:00 +0300

    добавили решения задачи 4

---
+++ Exp03/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("max: " + a);
+    }
+    else
+    {
+        Console.WriteLine("max: " + c);
+    }
+}
+
+else if (b > c)
+{
+    Console.WriteLine("max: " + b);
+}
+else
+{
+    Console.WriteLine("max: " + c);
+}
