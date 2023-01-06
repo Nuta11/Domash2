c0042a4578c6d251e265b3f6e6623216042bed13
Anna N <nearonskaya@yandex.ru>
2023-01-06 18:19:29 +0300

    добавила решение задачи 8

---
+++ Exp05/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+
+
+while(n <= N)
+    {
+        if(n % 2 ==0)
+        Console.Write(n + ", ");
+        n ++;
+    }
