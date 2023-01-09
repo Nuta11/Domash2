c1995febf4a338e0dc76b7ad1cd913be8715e5c6
Anna N <nearonskaya@yandex.ru>
2023-01-09 19:32:11 +0300

    создала решение и блок схему задачи 10

---
+++ Exp06/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
