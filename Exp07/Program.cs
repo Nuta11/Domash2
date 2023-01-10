2c0f3bcf6c81497d74d4f4c31100c540bf35ae59
Anna N <nearonskaya@yandex.ru>
2023-01-10 18:56:28 +0300

    записала решение задачи 13

---
+++ Exp07/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+Console.WriteLine("Третьей цифры нет");
+}
+
+while (number >= 1000)
+{
+  number = number / 10  ;
+}
+
+Console.WriteLine(number % 10);
