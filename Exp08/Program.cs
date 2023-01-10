c0fedd2f0b5a9828b6e32efd1b1cc93fe1a20e18
Anna N <nearonskaya@yandex.ru>
2023-01-10 22:33:43 +0300

    записала решение задачи 15

---
+++ Exp08/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+{
+if (day == 7 || day == 6)
+    Console.WriteLine("Выходной");
+
+else if (day >= 1 && day <= 5)
+    Console.WriteLine("Будни");
+
+else 
+    Console.WriteLine("Неверное число");
+}
+
+
+
+
+
+
+       
