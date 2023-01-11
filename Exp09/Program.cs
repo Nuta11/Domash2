9ed90b6357119e0ba47c239727f5d8dd89c106d2
Anna N <nearonskaya@yandex.ru>
2023-01-11 18:24:14 +0300

    написала решение задачи 19

---
+++ Exp09/Program.cs
@@ -0,0 +1,21 @@
+﻿//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine(Check(23432));
+
+bool Check(long val1)
+{
+long val2 = 0;
+long t = val1;
+
+while (val1 > 0)
+{
+  long o = val1 % 10;
+  val1 = val1 / 10;
+  val2 = val2 * 10 + o;
+}
+
+return t == val2;
+}
+
